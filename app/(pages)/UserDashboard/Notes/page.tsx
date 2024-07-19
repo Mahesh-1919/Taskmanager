b0ccbb8132a02920909b5c1049cf0efd586849a7
Mahesh-1919 <maheshjyothula23@gmail.com>
2024-07-19 18:05:48 +0530

    latest

--- app/(pages)/UserDashboard/Notes/page.tsx
+++ app/(pages)/UserDashboard/Notes/page.tsx
@@ -5,7 +5,7 @@ type Props = {};
 const Notes = (props: Props) => {
   return (
     <div className="text-white">
-      <h1 className="text-3xl font-bold mb-4 text-[#FEF2EA] mr-2">Notes</h1>
+      <h1 className="text-3xl font-bold mb-4 text-[#FEF2EA] mr-2">Notes </h1>
     </div>
   );
 };
